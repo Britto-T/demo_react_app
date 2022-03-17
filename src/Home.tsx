6069e47aaf805684b9ea3f83fa4bf50cdbd6f4aa
Britto-T <meetbrittohere@gmail.com>
2022-03-17 23:32:27 +0530

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
-import { useRef } from 'react'
+import { useState,useEffect,useRef} from 'react'
 
 const Home=()=>{
     const [name,setName] = useState("");
+    const inputRef = useRef<any>(1);
+
+    useEffect(()=>{
+        inputRef.current = inputRef.current+1;
+    });     
+
     return(
         <React.Fragment>
             <h4>I am a Home Page</h4>
             <label>User Name : </label>
-            <input type="text" name="User Name" onChange={(event:any)=>setName(event.target.value)} value={name}/>
+            <input type="text" ref={inputRef} name="User Name" onChange={(event:any)=>setName(event.target.value)} value={name}/>
             <p>The Entered name is {name} </p>
+            <p>The Number of times the page rendered is {inputRef.current}</p>
         </React.Fragment>
     )
 }
